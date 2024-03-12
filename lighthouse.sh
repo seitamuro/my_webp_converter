9b8d1a7f9ef4d9ae984d067bd45813ffe1af5526
seitamuro <87013401+seitamuro@users.noreply.github.com>
2024-03-12 16:55:46 +0900

    Update lighthouse.sh

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,2 +1,2 @@
-sudo npx lighthouse --output json --output-path ./report.json --chrome-flags="--headless --no-sandbox" http://54.91.76.60:80
+sudo npx lighthouse --output json --output-path ./report.json --chrome-flags="--headless --no-sandbox" $1
 
